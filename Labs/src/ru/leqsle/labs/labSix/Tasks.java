d4d963224cf42cd5501987e469c4d2b209440b3b
Alexandr Lebedev <perm.alexandr@gmail.com>
2021-04-10 18:44:46 +0500

    Информатика | Работа со структурами, выполнено две задачи

--- Labs/src/ru/leqsle/labs/labSix/Tasks.java
+++ Labs/src/ru/leqsle/labs/labSix/Tasks.java
@@ -1,6 +1,9 @@
 package ru.leqsle.labs.labSix;
 
 import ru.leqsle.labs.Task;
+import ru.leqsle.ui.Get;
+import ru.leqsle.ui.Input;
+import ru.leqsle.ui.Lines;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,62 +11,108 @@ import java.util.List;
 
 public class Tasks extends Task {
     private List<Pupil> pupils;
+    private Input string;
+    private int question;
+    private String addName;
+    private int addAge;
+    private float addRating;
+    private boolean ok;
+    private Lines line;
 
     public Tasks() throws IOException {
         super(1, "Формирование массива со структурой");
     }
 
     public void body() {
+        line = new Lines();
+        string = new Get();
         pupils = new ArrayList<>();
+        ok = true;
 
-        Pupil artem = new Pupil("Артем", 12, 10.4f);
-        Pupil dima = new Pupil("Дима", 9, 9.5f);
-        Pupil masha = new Pupil("Маша", 7, 15.4f);
-
+        Pupil artem = new Pupil("Артем", 12, 4f);
+        Pupil dima = new Pupil("Дима", 13, 5f);
+        Pupil masha = new Pupil("Маша", 7, 4f);
+        Pupil andrey = new Pupil("Андрей", 7, 3f);
+        Pupil sasha = new Pupil("Саша", 12, 4f);
+        Pupil lena = new Pupil("Лена", 13, 4.8f);
+        Pupil petya = new Pupil("Петя", 14, 3f);
+        Pupil sonya = new Pupil("Соня", 13, 4.9f);
 
         pupils.add(0, artem);
         pupils.add(1, dima);
         pupils.add(2, masha);
+        pupils.add(3, andrey);
+        pupils.add(4, sasha);
+        pupils.add(5, lena);
+        pupils.add(6, petya);
+        pupils.add(7, sonya);
 
-        for(Pupil i : pupils) {
-            System.out.printf("%s, %d, %f \n", i.getName(), i.getAge(), i.getRating());
+
+        while (ok) {
+            line.draw("-", 50);
+            System.out.println("1 - Добавить ученика\n2 - Показать список\n3 - Показать учеников 13 лет с рейтингом > 4\n4 - выход");
+            line.draw("-", 50);
+            question = string.getInt("Введите значение");
+
+            switch (question) {
+                case 1:
+                    addName = string.getString("Введите имя");
+                    addAge = string.getInt("Введите возраст");
+                    addRating = (float) string.getDouble("Введите рейтинг");
+                    pupils.add(new Pupil(addName, addAge, addRating));
+                    break;
+                case 2:
+                    for (Pupil i : pupils) {
+                        System.out.printf("%s, %d, %f \n", i.getName(), i.getAge(), i.getRating());
+                    }
+                    break;
+                case 3:
+                    for (Pupil i : pupils) {
+                        if (i.getAge() == 13 && i.getRating() > 4) {
+                            System.out.printf("%s, %d, %f \n", i.getName(), i.getAge(), i.getRating());
+                        }
+                    }
+                    break;
+                case 4:
+                    ok = false;
+            }
+
+        }
+    }
+
+    class Pupil {
+        private String name;
+        private int age;
+        private float rating;
+
+        public Pupil(String name, int age, float rating) {
+            setName(name);
+            setAge(age);
+            setRating(rating);
         }
 
-    }
-}
+        public String getName() {
+            return name;
+        }
 
-class Pupil {
-    private String name;
-    private int age;
-    private float rating;
+        public int getAge() {
+            return age;
+        }
 
-    public Pupil(String name, int age, float rating) {
-        setName(name);
-        setAge(age);
-        setRating(rating);
-    }
+        public float getRating() {
+            return rating;
+        }
 
-    public String getName() {
-        return name;
-    }
+        public void setName(String name) {
+            this.name = name;
+        }
 
-    public int getAge() {
-        return age;
-    }
+        public void setAge(int age) {
+            this.age = age;
+        }
 
-    public float getRating() {
-        return rating;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setRating(float rating) {
-        this.rating = rating;
+        public void setRating(float rating) {
+            this.rating = rating;
+        }
     }
 }
